5ccb4016390555a183133a80aa823b5a2fe790f6
lalbertogith <lmoranchel@gmail.com>
2017-02-04 22:07:41 +0100

    Initial commit

---
+++ app/src/main/java/com/k2tic/MainActivity.java
@@ -0,0 +1,15 @@
+package com.k2tic;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.k2tic.courserabienvenido.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
